c4269744cd3a22dbd23eb9c32996e4a7a15823ac
elaine <766205010@qq.com>
2024-05-27 16:46:00 +0800

    合理地触发响应---响应系统的作用与实现

---
+++ chapter5/5.4/index.js
@@ -0,0 +1,128 @@
+let activeEffect = null
+const effectStack = []
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn) // 清空依赖
+        // effect嵌套的处理 *开始*
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        const res = fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+        // effect嵌套的处理 *结束*
+        return res
+    }
+    effectFn.options = options
+    effectFn.deps = []
+    if (!options.lazy) {
+        effectFn()
+    }
+    return effectFn
+}
+
+/**
+ * 清理 effectFn 的依赖关系。
+ * @param {Function} effectFn - 具有依赖关系的函数，其结构包含一个 deps 数组，用于存储依赖项。
+ */
+function cleanup(effectFn) {
+    // 遍历 effectFn 的所有依赖
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i]
+        // 从依赖项中删除 effectFn
+        deps.delete(effectFn)
+    }
+    // 清空 effectFn 的依赖数组
+    effectFn.deps.length = 0
+}
+
+const bucket = new WeakMap()
+
+function track(target, key) {
+    if (!activeEffect) return
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()))
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+    activeEffect.deps.push(deps)
+}
+function trigger(target, key) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const deps = depsMap.get(key)
+    const depsToRun = new Set()
+    deps &&
+        deps.forEach((effectFn) => {
+            // 如果trigger触发执行的副作用函数与当前正在执行的副作用函数相同，
+            // 则不触发执行
+            if (effectFn !== activeEffect) {
+                depsToRun.add(effectFn)
+            }
+        })
+
+    depsToRun.forEach((effect) => {
+        if (effect.options.scheduler) {
+            effect.options.scheduler(effect)
+        } else {
+            effect()
+        }
+    })
+}
+
+// 存储副作用函数的桶
+
+// 原始数据
+const data = {
+    foo: 1,
+}
+
+function reactive(data) {
+    return new Proxy(data, {
+        // 拦截读取操作
+        get(target, key, receiver) {
+            track(target, key)
+            // 返回属性值
+            return Reflect.get(target, key, receiver)
+        },
+        // 拦截设置操作
+        set(target, key, newVal, receiver) {
+            // 设置属性值
+            const oldVal = target[key]
+            const res = Reflect.set(target, key, newVal, receiver)
+            if (oldVal !== newVal) {
+                trigger(target, key)
+            }
+            // 返回 true 代表设置操作成功
+            return res
+        },
+    })
+}
+
+/*
+// 对原始数据的代理
+const obj = reactive(data)
+
+effect(() => {
+    console.log(obj.foo)
+})
+
+// 设置 p.foo 的值，但值没有变化。
+// 期望不触发响应
+obj.foo = 1
+*/
+
+const temp = {}
+const proto = { bar: 1 }
+const child = reactive(temp)
+const parent = reactive(proto)
+Object.setPrototypeOf(child, parent)
+effect(() => {
+    // 收集child、parent的依赖
+    console.log(child.bar)
+})
+// 由于child没有bar属性，所以也会触发parent的set
+child.bar = 2
