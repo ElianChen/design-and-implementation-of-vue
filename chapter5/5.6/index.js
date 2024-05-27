aa7b26d311f56e15aa471674d50356519d389d94
elaine <766205010@qq.com>
2024-05-27 16:46:30 +0800

    只读---响应系统的作用与实现

---
+++ chapter5/5.6/index.js
@@ -0,0 +1,160 @@
+let activeEffect = null
+const effectStack = []
+function effect(fn, options = {}) {
+    const effectFn = () => {
+        cleanup(effectFn) // 清空依赖
+        // effect嵌套的处理 *开始*
+        activeEffect = effectFn
+        effectStack.push(effectFn)
+        const res = fn()
+        effectStack.pop()
+        activeEffect = effectStack[effectStack.length - 1]
+        // effect嵌套的处理 *结束*
+        return res
+    }
+    effectFn.options = options
+    effectFn.deps = []
+    if (!options.lazy) {
+        effectFn()
+    }
+    return effectFn
+}
+
+/**
+ * 清理 effectFn 的依赖关系。
+ * @param {Function} effectFn - 具有依赖关系的函数，其结构包含一个 deps 数组，用于存储依赖项。
+ */
+function cleanup(effectFn) {
+    // 遍历 effectFn 的所有依赖
+    for (let i = 0; i < effectFn.deps.length; i++) {
+        const deps = effectFn.deps[i]
+        // 从依赖项中删除 effectFn
+        deps.delete(effectFn)
+    }
+    // 清空 effectFn 的依赖数组
+    effectFn.deps.length = 0
+}
+
+const bucket = new WeakMap()
+
+function track(target, key) {
+    if (!activeEffect) return
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        bucket.set(target, (depsMap = new Map()))
+    }
+    let deps = depsMap.get(key)
+    if (!deps) {
+        depsMap.set(key, (deps = new Set()))
+    }
+    deps.add(activeEffect)
+    activeEffect.deps.push(deps)
+}
+function trigger(target, key, type) {
+    const depsMap = bucket.get(target)
+    if (!depsMap) return
+    const deps = depsMap.get(key)
+    const depsToRun = new Set()
+    deps &&
+        deps.forEach((effectFn) => {
+            // 如果trigger触发执行的副作用函数与当前正在执行的副作用函数相同，
+            // 则不触发执行
+            if (effectFn !== activeEffect) {
+                depsToRun.add(effectFn)
+            }
+        })
+
+    if (type === 'ADD' || type === 'DELETE') {
+        const iterateEffects = depsMap.get(ITERATE_KEY)
+        iterateEffects &&
+            iterateEffects.forEach((effectFn) => {
+                if (effectFn !== activeEffect) {
+                    depsToRun.add(effectFn)
+                }
+            })
+    }
+
+    depsToRun.forEach((effect) => {
+        if (effect.options.scheduler) {
+            effect.options.scheduler(effect)
+        } else {
+            effect()
+        }
+    })
+}
+
+// 存储副作用函数的桶
+
+// 原始数据
+const data = {
+    foo: {
+        bar: 1,
+    },
+}
+
+const ITERATE_KEY = Symbol()
+
+function createReactive(data, isShallow = false, isReadonly = false) {
+    return new Proxy(data, {
+        // 拦截读取操作
+        get(target, key, receiver) {
+            if (key === 'raw') {
+                return target
+            }
+            if (!isReadonly) {
+                track(target, key)
+            }
+            // 返回属性值
+            const res = Reflect.get(target, key, receiver)
+            if (isShallow) {
+                return res
+            }
+            if (typeof res === 'object' && res !== null) {
+                return createReactive(res)
+            }
+            return res
+        },
+        // 拦截设置操作
+        set(target, key, newVal, receiver) {
+            if (isReadonly) {
+                console.warn(`属性${key}是只读的`)
+                return true
+            }
+            // 设置属性值
+            const oldVal = target[key]
+            const type = Object.prototype.hasOwnProperty.call(target, key)
+                ? 'SET'
+                : 'ADD'
+            const res = Reflect.set(target, key, newVal, receiver)
+            if (target === receiver.raw) {
+                if (oldVal !== newVal) {
+                    trigger(target, key, type)
+                }
+            }
+            // 返回 true 代表设置操作成功
+            return res
+        },
+        ownKeys(target) {
+            track(target, ITERATE_KEY)
+            return Reflect.ownKeys(target)
+        },
+        deleteProperty(target, key) {
+            if (isReadonly) {
+                console.warn(`属性${key}是只读的`)
+                return true
+            }
+            const hasKey = Object.prototype.hasOwnProperty.call(target, key)
+            const res = Reflect.deleteProperty(target, key)
+            if (hasKey && res) {
+                trigger(target, key, 'DELETE')
+            }
+            return res
+        },
+    })
+}
+
+const obj = createReactive(data, false)
+effect(() => {
+    console.log(obj.foo.bar)
+})
+obj.foo.bar = 2
